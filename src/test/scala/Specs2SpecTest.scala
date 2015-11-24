d7d2ef56115514fa10a56c4f3e6c3d14f8a5fac7
Sean Hester <sean.hester@bettercloud.com>
2015-11-24 18:56:33 -0500

    Added simple Scala class and two example test classes: one written for Specs2 and the other written for ScalaTest;
    Signed-off-by: Sean Hester <sean.hester@bettercloud.com>

---
+++ src/test/scala/Specs2SpecTest.scala
@@ -0,0 +1,12 @@
+import org.specs2._
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class QuickStartSpec extends Specification { def is = s2"""
+
+ This is my first specification
+   it is working                 $ok
+   really working!               $ok
+                                 """
+}
